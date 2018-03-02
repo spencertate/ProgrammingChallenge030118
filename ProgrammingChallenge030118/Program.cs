48691dc4e3b2e68e556a3def5e8902cb73a3a112
Spencer Tate <shoit718@go.byuh.edu>
2018-03-01 18:31:17 -1000

    Creation and Upload

---
+++ ProgrammingChallenge030118/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingChallenge030118
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] output = AddFillers("Mr John Smith    ".ToArray<char>(), 13);
+            foreach(char c in output)
+            {
+                Console.Write(c);
+            }
+            Console.Read();
+        }
+
+        public static char[] AddFillers(char[] iArray, int trueLength)
+        {
+            int trueTrack = trueLength - 1;
+            for(int i = iArray.Length-1; i >= 0; i--)
+            {
+                if(iArray[trueTrack] != ' ')
+                {
+                    iArray[i] = iArray[trueTrack];    
+                }
+                else
+                {
+                    iArray[i] = '0';
+                    i--;
+                    iArray[i] = '2';
+                    i--;
+                    iArray[i] = '%';
+                }
+                trueTrack--;
+            }
+            return iArray;
+        }
+    }
+}
